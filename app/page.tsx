0ffa19c88e855222a56a42d28cddb8caef698d31
trankil123 <72249795+trankil123@users.noreply.github.com>
2025-10-12 14:33:32 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const frame = {
     title: 'Launch Template',
     action: {
       type: 'launch_frame',
-      name: 'Monad Farcaster MiniApp Template',
+      name: 'Trankil Farcaster MiniApp',
       url: APP_URL,
       splashImageUrl: `${APP_URL}/images/splash.png`,
       splashBackgroundColor: '#f7f7f7',
@@ -19,10 +19,10 @@ const frame = {
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: 'Monad Farcaster MiniApp Template',
+    title: 'Trankil Farcaster MiniApp',
     openGraph: {
-      title: 'Monad Farcaster MiniApp Template',
-      description: 'A template for building mini-apps on Farcaster and Monad',
+      title: 'Trankil Farcaster MiniApp',
+      description: 'A template for building mini-apps on Farcaster and Base',
     },
     other: {
       'fc:frame': JSON.stringify(frame),
