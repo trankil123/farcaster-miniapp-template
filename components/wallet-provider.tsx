a39eccc80b182332f6809c43193385ed933d7544
trankil123 <72249795+trankil123@users.noreply.github.com>
2025-10-12 15:19:35 +0200

    Update wallet-provider.tsx

--- components/wallet-provider.tsx
+++ components/wallet-provider.tsx
@@ -1,7 +1,8 @@
 import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { http, WagmiProvider, createConfig } from 'wagmi'
-import { monadTestnet } from 'wagmi/chains'
+//import { monadTestnet } from 'wagmi/chains'
+import { base } from 'wagmi/chains'
 
 export const config = createConfig({
  //chains: [monadTestnet],
