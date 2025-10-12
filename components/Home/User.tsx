c6b2da72d1bfca711fffe050b7f4708346939f3e
trankil123 <72249795+trankil123@users.noreply.github.com>
2025-10-12 17:19:44 +0200

    Update User.tsx

--- components/Home/User.tsx
+++ components/Home/User.tsx
@@ -5,7 +5,7 @@ export function User() {
 
   return (
     <div className="space-y-4 border border-[#333] rounded-md p-4">
-      <h2 className="text-xl font-bold text-left">sdk.context</h2>
+      <h2 className="text-xl font-bold text-left">Your profile</h2>
       <div className="flex flex-row space-x-4 justify-start items-start">
         {context?.user ? (
           <>
